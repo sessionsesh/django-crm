3a411bf31419ed6edea2506c3d5e563521b01896
sessionsesh <sessionsesh@gmail.com>
2021-07-26 15:35:30 +0300

    Make changes in Telegram Thread class and Signals

--- crm/telegram_notifications/apps.py
+++ crm/telegram_notifications/apps.py
@@ -2,24 +2,33 @@ from django.apps import AppConfig
 import threading as th
 import time
 import sys
+from dotenv import load_dotenv
+from os import environ
+from telegram_notifications.bot import TelegramBot
+
 
 class BotLongPollThread(th.Thread):
     """
     Thread that handles Telegram Bot income messages
     """
-    def __init__(self, running=True, daemon=True):
+
+    def __init__(self, bot_token, running=True, daemon=True):
         super(BotLongPollThread, self).__init__()
+        self.telegramBot = TelegramBot(bot_token)
         self.running = running
         self.daemon = daemon # allows to use 'ctrl+c' to close process
 
     def run(self):
         print('|Telegram notification module is running|')
-        while(self.running):
-            # Bot code
-            time.sleep(0)# without it doesn't work
+        while self.running:
+            print(self.telegramBot.getUpdates()) # without it doesn't work
+            time.sleep(5)
 
     def setRunning(self, value):
         self.running = value
+
+load_dotenv()
+botThread = BotLongPollThread(environ['BOT_TOKEN']) # sharing between modules
 
 class TelegramNotificationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -27,8 +36,8 @@ class TelegramNotificationsConfig(AppConfig):
 
     def ready(self):
         import telegram_notifications.signals   # importing signals
+
         if 'runserver' in sys.argv:
-            botThread = BotLongPollThread()
-            #botThread.start()
+            botThread.start()
 
         return super(TelegramNotificationsConfig, self).ready()
--- crm/telegram_notifications/signals.py
+++ crm/telegram_notifications/signals.py
@@ -3,10 +3,20 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from accounts.models import User
-from crm.telegram_notifications import bot
 
+from telegram_notifications.apps import botThread
+
+
+chat_id = '231731765'
+
+@receiver(request_finished)
+def my_handler(sender, **kwargs):
+    text = 'Request finished'
+    botThread.telegramBot.sendMessage(chat_id, text)
+    print('SIGNAL_REQUEST_SENT')
 
 @receiver(post_save)
-def update_order(sender, instance, **kwargs):
-    print(User.objects.all().first())
-    print("Request finished!")
+def my_db_handler(sender, instance, **kwargs):
+    text='DB CHANGED'
+    botThread.telegramBot.sendMessage(chat_id, text)
+    print('SIGNAL_DB_SENT')
